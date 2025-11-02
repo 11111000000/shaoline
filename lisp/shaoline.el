76d89682f1b523f931b1bf0bc5e770d1296649d0
Peter Kosov <11111000000@email.com>
2025-11-02 20:33:49 +0800

    chore: bump shaoline.el version to 3.3.4 to reflect updates

--- lisp/shaoline.el
+++ lisp/shaoline.el
@@ -1,6 +1,6 @@
 ;;; shaoline.el --- Functional minimalist echo-area modeline -*- lexical-binding: t; -*-
 
-;; Version: 3.3.3
+;; Version: 3.3.4
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, mode-line, battery, clock, git, gptel, moon, project, tao
 ;; Copyright (C) 2025 Peter
